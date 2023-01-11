0836669ec3b0e2f7f372ec55d33c6d110d51d0de
Wyatt-Stanke <wyattstanke@icloud.com>
2023-01-11 17:54:06 +0000

    Sketch out a little more

--- boost-crawl/src/lib.rs
+++ boost-crawl/src/lib.rs
@@ -1,4 +1,7 @@
+use std::collections::HashMap;
+
 use lazy_static::lazy_static;
+use maplit::hashmap;
 use reqwest::Response;
 // use scraper::Html;
 use regex::Regex;
@@ -10,8 +13,30 @@ pub async fn extract_links(res: Response) -> Vec<String> {
     // let html = Html::parse_document(&res.text().await.unwrap());
     URL_REGEX
         .find_iter(&res.text().await.unwrap())
-        .filter_map(|digits| digits.as_str().parse().ok())
+        .map(|digits| defang(digits.as_str()))
         .collect()
+}
+
+fn defang(text: &str) -> String {
+    let defangs: HashMap<&str, &str> = hashmap! {
+        " (dot) " => ".",
+        "(dot)" => ".",
+        " (at) " => "@",
+        "(at)" => "@",
+        "[.]" => ".",
+        "[&]" => "&",
+        "[@]" => "@",
+        "[/]" => "/",
+        "hxxps" => "https",
+        "hxxp" => "http",
+        "fxps" => "ftps",
+        "fxp" => "ftp"
+    };
+    let mut ans = text.to_owned();
+    for (df, f) in defangs.into_iter() {
+        ans = ans.replace(df, f);
+    }
+    ans
 }
 
 #[cfg(test)]
